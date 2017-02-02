cfa41a13b217a73070596fb3f620123b9d3689cf
rbnschlz <rbnschlz@gmail.com>
2017-02-02 10:19:36 +0100

    Initial Installation

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+
+const ExtractScreenCSS = new ExtractTextPlugin({ filename: "../css/main.css" });
+
+
+module.exports = {
+	context: __dirname + "/assets",
+	entry: './js/input.js',
+	output: {
+		path: __dirname + '/assets/js',
+		filename: 'output.js'
+	},
+
+	module: {
+        loaders: [
+            { test: /\.less$/, loader: ExtractScreenCSS.extract({
+                fallbackLoader: "style-loader",
+                loader: "css?minimize!postcss!less"
+            }) }
+        ]
+    },
+
+    plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			_: "lodash"
+		}),
+
+		new webpack.optimize.LimitChunkCountPlugin({maxChunks: 0}),
+
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			},
+			output: {
+			    comments: false
+			},
+		}),
+
+		ExtractScreenCSS
+	],
+};
+
+
+
