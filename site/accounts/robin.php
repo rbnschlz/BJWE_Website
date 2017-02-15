4a48e04a9078075068343d2279998e27b4bd10ac
rbnschlz <rbnschlz@gmail.com>
2017-02-15 18:18:37 +0100

    Update

--- site/accounts/robin.php
+++ site/accounts/robin.php
@@ -2,10 +2,12 @@
 
 username: robin
 password: >
-  $2a$10$0YeVBGZpxr8UpNOztDD5keLQ.mIa3JccwTIbf7cxMwerswlRmTb6e
+  $2a$10$gow4YDW2vr./PwFvQNZ4fuesDFTYYPebAbsCVV8M6VEqN6EJF8ZqS
 email: rs@robinscholz.com
 language: en
 role: admin
 history:
   - contact
   - home
+firstname: ""
+lastname: ""
