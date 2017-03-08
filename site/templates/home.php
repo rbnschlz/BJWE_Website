10b31db1621ea511844072208475c493b5ca2983
rbnschlz <rbnschlz@gmail.com>
2017-03-08 06:40:33 +0100

    Bug Fixes

--- site/templates/home.php
+++ site/templates/home.php
@@ -107,6 +107,26 @@
 	echo $block;
 	?>
 </div>
+
+<!-- SVG filter for Firefox  -->
+<!-- <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<defs>
+    <filter id="blurry">
+     	<feGaussianBlur stdDeviation="5"/>
+    </filter>
+    <filter id="drop-shadow">
+	    <feGaussianBlur in="SourceAlpha" stdDeviation="2"/>
+	    <feOffset dx="5" dy="5" result="offsetblur"/>
+	    <feFlood flood-color="#000000"/>
+	    <feComposite in2="offsetblur" operator="in"/>
+	    <feMerge>
+		    <feMergeNode/>
+		    <feMergeNode in="SourceGraphic"/>
+	    </feMerge>
+	</filter>
+</defs>
+</svg>	 -->
+
 <div class="info_wrapper hidden">
 	<div class="info_contact">
 		<span><a href="mailto:<?php echo $contact->mail(); ?>"><?php echo $contact->mail(); ?></a>, </span>
@@ -118,20 +138,20 @@
 		<?php if ($contact->represent()->isNotEmpty()) {
 			$block = "<div class='info_represent'>";
 			foreach($contact->represent()->toStructure() as $agent) {
-				$block .= "<li>Represented";
+				$block .= "<span>Represented";
 				$block .= $agent->location()->isNotEmpty() ? " in {$agent->location()} by " : " by ";
 				$block .= "<a href='";
 				$block .= $agent->url();
 				$block .= "' target='_blank'>";
 				$block .= "<span class='nobr'>{$agent->name()}</span>";
-				$block .= "</a></li>";
+				$block .= "</a></span>";
 			}
 			$block .= "</div>";
 
 			echo $block;
 			};
 			if ($contact->instagram()->isNotEmpty()) { 
-				$block = "<a href='";
+				$block = "<a class='headline' href='";
 				$block .= $contact->instagram()->html();
 				$block .= "' target='_blank'>Instagram</a>";
 
